514fe5817e29bff8ad6f717af4c19ab6fb46e6e0
lee-jeong-geun <ljk0411jg@naver.com>
2020-03-10 04:45:34 +0900

    add TodoDao

---
+++ todo/src/main/java/kr/or/connect/todo/dao/TodoDao.java
@@ -0,0 +1,22 @@
+package kr.or.connect.todo.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.or.connect.todo.dto.TodoDto;
+
+public class TodoDao {
+	
+	public int addTodo(TodoDto todo) {
+		return 0;
+	}
+
+	public List<TodoDto> getTodos() {
+		List<TodoDto> list = new ArrayList<>();
+		return list;
+	}
+	
+	public int updateTodo(TodoDto todo) {
+		return 0;
+	}
+}
