6d7261dacae25b0e01cc0abe157db835873e5dac
lee-jeong-geun <ljk0411jg@naver.com>
2020-03-10 06:23:54 +0900

    add getTodos()

--- todo/src/main/java/kr/or/connect/todo/dao/TodoDao.java
+++ todo/src/main/java/kr/or/connect/todo/dao/TodoDao.java
@@ -1,21 +1,59 @@
 package kr.or.connect.todo.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
 import kr.or.connect.todo.dto.TodoDto;
 
 public class TodoDao {
-	
+	private static String dbUrl = "jdbc:mysql://localhost:3306/connectdb";
+	private static String dbUser = "connectuser";
+	private static String dbPasswd = "connect123!@#";
+
 	public int addTodo(TodoDto todo) {
 		return 0;
 	}
 
 	public List<TodoDto> getTodos() {
 		List<TodoDto> list = new ArrayList<>();
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String sql = "select * from todo";
+		try (Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPasswd);
+				PreparedStatement ps = conn.prepareStatement(sql)) {
+
+			try (ResultSet rs = ps.executeQuery()) {
+
+				while (rs.next()) {
+					Long id = rs.getLong("id");
+					String title = rs.getString("title");
+					String name = rs.getString("name");
+					int sequence = rs.getInt("sequence");
+					String type = rs.getString("type");
+					String regDate = rs.getString("regdate");
+					TodoDto todoDto = new TodoDto(id, name, regDate, sequence, title, type);
+					list.add(todoDto);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return list;
 	}
-	
+
 	public int updateTodo(TodoDto todo) {
 		return 0;
 	}
