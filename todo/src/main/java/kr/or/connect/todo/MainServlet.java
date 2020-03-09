f685997191b640df30c982f7438540a9fad100e6
lee-jeong-geun <ljk0411jg@naver.com>
2020-03-10 06:25:35 +0900

    create MainServlet

---
+++ todo/src/main/java/kr/or/connect/todo/MainServlet.java
@@ -0,0 +1,32 @@
+package kr.or.connect.todo;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.connect.todo.dao.TodoDao;
+import kr.or.connect.todo.dto.TodoDto;
+
+@WebServlet("/main")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		TodoDao dao = new TodoDao();
+		List<TodoDto> list = dao.getTodos();
+		
+		request.setAttribute("todoList", list);
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("main.jsp");
+		requestDispatcher.forward(request, response);
+	}
+
+}
