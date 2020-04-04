2054bcf8fd01403d4c76c1288408dd2425fabfa8
lee-jeong-geun <ljk0411jg@naver.com>
2020-04-05 00:01:49 +0900

    code refactoring

--- todo/src/main/java/kr/or/connect/todo/dao/TodoDao.java
+++ todo/src/main/java/kr/or/connect/todo/dao/TodoDao.java
@@ -10,12 +10,12 @@ import java.util.List;
 import kr.or.connect.todo.dto.TodoDto;
 
 public class TodoDao {
-	private static final String dbUrl = "jdbc:mysql://localhost:3306/connectdb?characterEncoding=UTF-8";
-	private static final String dbUser = "connectuser";
-	private static final String dbPasswd = "connect123!@#";
-	private static final String addTodoSql = "insert into todo(title, name, sequence) values(?, ?, ?)";
-	private static final String getTodosSql = "select * from todo order by id";
-	private static final String updateTodoSql = "update todo set type = ? where id = ?";
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/connectdb?characterEncoding=UTF-8";
+	private static final String DB_USER = "connectuser";
+	private static final String DB_PASSWD = "connect123!@#";
+	private static final String ADD_TODO_SQL = "insert into todo(title, name, sequence) values(?, ?, ?)";
+	private static final String GET_TODOS_SQL = "select * from todo order by id";
+	private static final String UPDATE_TODO_SQL = "update todo set type = ? where id = ?";
 
 	static {
 		try {
@@ -28,8 +28,8 @@ public class TodoDao {
 	public int addTodo(TodoDto todo) {
 		int addCount = 0;
 
-		try (Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPasswd);
-				PreparedStatement ps = conn.prepareStatement(addTodoSql)) {
+		try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWD);
+				PreparedStatement ps = conn.prepareStatement(ADD_TODO_SQL)) {
 
 			ps.setString(1, todo.getTitle());
 			ps.setString(2, todo.getName());
@@ -47,23 +47,19 @@ public class TodoDao {
 	public List<TodoDto> getTodos() {
 		List<TodoDto> list = new ArrayList<>();
 
-		try (Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPasswd);
-				PreparedStatement ps = conn.prepareStatement(getTodosSql)) {
+		try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWD);
+				PreparedStatement ps = conn.prepareStatement(GET_TODOS_SQL);
+				ResultSet rs = ps.executeQuery()) {
 
-			try (ResultSet rs = ps.executeQuery()) {
-
-				while (rs.next()) {
-					Long id = rs.getLong("id");
-					String title = rs.getString("title");
-					String name = rs.getString("name");
-					int sequence = rs.getInt("sequence");
-					String type = rs.getString("type");
-					String regDate = rs.getDate("regdate").toString();
-					TodoDto todoDto = new TodoDto(id, name, regDate, sequence, title, type);
-					list.add(todoDto);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
+			while (rs.next()) {
+				Long id = rs.getLong("id");
+				String title = rs.getString("title");
+				String name = rs.getString("name");
+				int sequence = rs.getInt("sequence");
+				String type = rs.getString("type");
+				String regDate = rs.getDate("regdate").toString();
+				TodoDto todoDto = new TodoDto(id, name, regDate, sequence, title, type);
+				list.add(todoDto);
 			}
 
 		} catch (Exception e) {
@@ -76,8 +72,8 @@ public class TodoDao {
 	public int updateTodo(TodoDto todo) {
 		int updateCount = 0;
 
-		try (Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPasswd);
-				PreparedStatement ps = conn.prepareStatement(updateTodoSql)) {
+		try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWD);
+				PreparedStatement ps = conn.prepareStatement(UPDATE_TODO_SQL)) {
 			ps.setString(1, todo.getType());
 			ps.setLong(2, todo.getId());
 
