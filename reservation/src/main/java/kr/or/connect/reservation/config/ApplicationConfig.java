485b3b46df6da7597ef291d8f76b91acd1b7351e
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-20 04:10:54 +0900

    add PropertySource

--- reservation/src/main/java/kr/or/connect/reservation/config/ApplicationConfig.java
+++ reservation/src/main/java/kr/or/connect/reservation/config/ApplicationConfig.java
@@ -3,9 +3,11 @@ package kr.or.connect.reservation.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = {"kr.or.connect.reservation.dao", "kr.or.connect.reservation.service"})
 @Import({DBConfig.class})
+@PropertySource("application.properties")
 public class ApplicationConfig {
 }
