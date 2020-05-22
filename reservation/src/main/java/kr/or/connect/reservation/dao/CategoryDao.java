9e618c0ee7de454cc9929b058fa7c4be0f50ce3e
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-23 03:16:46 +0900

    add constructor, selectAll

--- reservation/src/main/java/kr/or/connect/reservation/dao/CategoryDao.java
+++ reservation/src/main/java/kr/or/connect/reservation/dao/CategoryDao.java
@@ -1,4 +1,26 @@
 package kr.or.connect.reservation.dao;
 
+import kr.or.connect.reservation.dto.Category;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+import static kr.or.connect.reservation.dao.CategoryDaoSqls.*;
+
+@Repository
 public class CategoryDao {
+    private NamedParameterJdbcTemplate jdbc;
+    private RowMapper<Category> rowMapper = BeanPropertyRowMapper.newInstance(Category.class);
+
+    public CategoryDao(DataSource dataSource) {
+        this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    public List<Category> selectAll() {
+        return jdbc.query(SELECT_SQL, rowMapper);
+    }
 }
