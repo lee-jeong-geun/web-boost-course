326a3697d5b05e36e8a458d2a83acf9ca48dd110
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-23 23:46:03 +0900

    modify SELECT_SQL

--- reservation/src/main/java/kr/or/connect/reservation/dao/CategoryDaoSqls.java
+++ reservation/src/main/java/kr/or/connect/reservation/dao/CategoryDaoSqls.java
@@ -1,5 +1,5 @@
 package kr.or.connect.reservation.dao;
 
 public class CategoryDaoSqls {
-    public static final String SELECT_SQL = "select id, name from category";
+    public static final String SELECT_SQL = "select t1.id, t1.name, count(*) as count from category as t1 join product as t2 on t1.id = t2.category_id group by t1.id";
 }
