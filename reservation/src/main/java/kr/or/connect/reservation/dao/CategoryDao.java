7c7229758c61c33970e7c60d9fd15901fd736535
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-23 23:51:39 +0900

    modify Dto

--- reservation/src/main/java/kr/or/connect/reservation/dao/CategoryDao.java
+++ reservation/src/main/java/kr/or/connect/reservation/dao/CategoryDao.java
@@ -1,6 +1,6 @@
 package kr.or.connect.reservation.dao;
 
-import kr.or.connect.reservation.dto.Category;
+import kr.or.connect.reservation.dto.CategoryDto;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -9,18 +9,18 @@ import org.springframework.stereotype.Repository;
 import javax.sql.DataSource;
 import java.util.List;
 
-import static kr.or.connect.reservation.dao.CategoryDaoSqls.*;
+import static kr.or.connect.reservation.dao.CategoryDaoSqls.SELECT_SQL;
 
 @Repository
 public class CategoryDao {
     private NamedParameterJdbcTemplate jdbc;
-    private RowMapper<Category> rowMapper = BeanPropertyRowMapper.newInstance(Category.class);
+    private RowMapper<CategoryDto> rowMapper = BeanPropertyRowMapper.newInstance(CategoryDto.class);
 
     public CategoryDao(DataSource dataSource) {
         this.jdbc = new NamedParameterJdbcTemplate(dataSource);
     }
 
-    public List<Category> selectAll() {
+    public List<CategoryDto> selectAll() {
         return jdbc.query(SELECT_SQL, rowMapper);
     }
 }
