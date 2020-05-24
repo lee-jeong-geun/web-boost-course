5f52cfd73781a7016699c8335465ee68e80211a6
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-24 18:21:56 +0900

    modify GetMapping, add constructor

--- reservation/src/main/java/kr/or/connect/reservation/controller/CategoryController.java
+++ reservation/src/main/java/kr/or/connect/reservation/controller/CategoryController.java
@@ -1,11 +1,12 @@
 package kr.or.connect.reservation.controller;
 
-import org.springframework.ui.ModelMap;
+import kr.or.connect.reservation.dto.CategoryDto;
+import kr.or.connect.reservation.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,14 +14,18 @@ import java.util.Map;
 @RestController
 @RequestMapping(path = "/api/categories")
 public class CategoryController {
-    @GetMapping
-    public Map<String, Object> categories(ModelMap model) {
 
+    private CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public Map<String, Object> categories() {
         Map<String, Object> map = new HashMap<>();
-        List<String> list = new ArrayList<>();
-        list.add("id");
-        list.add("name");
-        list.add("count");
+        List<CategoryDto> list = categoryService.getCategories();
         map.put("items", list);
         return map;
     }
