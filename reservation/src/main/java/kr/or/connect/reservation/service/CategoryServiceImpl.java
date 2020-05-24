6ef4640f95528c0f2deb18e460ade8331f901b22
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-24 18:28:44 +0900

    add getCategories, constructor

--- reservation/src/main/java/kr/or/connect/reservation/service/CategoryServiceImpl.java
+++ reservation/src/main/java/kr/or/connect/reservation/service/CategoryServiceImpl.java
@@ -1,7 +1,24 @@
 package kr.or.connect.reservation.service;
 
+import kr.or.connect.reservation.dao.CategoryDao;
+import kr.or.connect.reservation.dto.CategoryDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class CategoryServiceImpl {
+public class CategoryServiceImpl implements CategoryService {
+    
+    CategoryDao categoryDao;
+
+    @Autowired
+    public CategoryServiceImpl(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }
+
+    @Override
+    public List<CategoryDto> getCategories() {
+        return categoryDao.selectAll();
+    }
 }
