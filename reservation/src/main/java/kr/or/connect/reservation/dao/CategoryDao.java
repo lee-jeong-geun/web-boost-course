061e28e426e72e2fb87b77717a589d8a8155f05a
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-25 18:52:48 +0900

    add selectCount

--- reservation/src/main/java/kr/or/connect/reservation/dao/CategoryDao.java
+++ reservation/src/main/java/kr/or/connect/reservation/dao/CategoryDao.java
@@ -7,9 +7,12 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
-import static kr.or.connect.reservation.dao.CategoryDaoSqls.SELECT_SQL;
+import static kr.or.connect.reservation.dao.CategoryDaoSqls.SELECT_ALL_SQL;
+import static kr.or.connect.reservation.dao.CategoryDaoSqls.SELECT_COUNT_SQL;
 
 @Repository
 public class CategoryDao {
@@ -21,6 +24,11 @@ public class CategoryDao {
     }
 
     public List<CategoryDto> selectAll() {
-        return jdbc.query(SELECT_SQL, rowMapper);
+        return jdbc.query(SELECT_ALL_SQL, rowMapper);
+    }
+
+    public int selectCount(int id) {
+        Map<String, ?> params = Collections.singletonMap("id", id);
+        return jdbc.queryForObject(SELECT_COUNT_SQL, params, Integer.class);
     }
 }
