0357c7bdf89fe3429c1a81acb69c92f494d52805
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-26 00:42:19 +0900

    add selectById

--- reservation/src/main/java/kr/or/connect/reservation/dao/ProductDao.java
+++ reservation/src/main/java/kr/or/connect/reservation/dao/ProductDao.java
@@ -1,4 +1,35 @@
 package kr.or.connect.reservation.dao;
 
+import kr.or.connect.reservation.dto.ProductDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static kr.or.connect.reservation.dao.ProductDaoSqls.SELECT_PRODUCT_SQL;
+
+@Repository
 public class ProductDao {
+    private NamedParameterJdbcTemplate jdbc;
+    private RowMapper<ProductDto> rowMapper = BeanPropertyRowMapper.newInstance(ProductDto.class);
+
+    @Autowired
+    public ProductDao(DataSource dataSource) {
+        this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    public List<ProductDto> selectById(int id, int start, int limit) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", id);
+        params.put("start", start);
+        params.put("limit", limit);
+        return jdbc.query(SELECT_PRODUCT_SQL, params, rowMapper);
+    }
 }
