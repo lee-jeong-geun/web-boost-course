d7c53bc504c79dfa172b99a86f70cbcfae36c24b
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-26 00:41:31 +0900

    add getProdcuts

--- reservation/src/main/java/kr/or/connect/reservation/service/ProductService.java
+++ reservation/src/main/java/kr/or/connect/reservation/service/ProductService.java
@@ -1,4 +1,9 @@
 package kr.or.connect.reservation.service;
 
+import kr.or.connect.reservation.dto.ProductDto;
+
+import java.util.List;
+
 public interface ProductService {
+    List<ProductDto> getProducts(int id, int start, int limit);
 }
--- reservation/src/main/java/kr/or/connect/reservation/service/ProductServiceImpl.java
+++ reservation/src/main/java/kr/or/connect/reservation/service/ProductServiceImpl.java
@@ -1,4 +1,24 @@
 package kr.or.connect.reservation.service;
 
-public class ProductServiceImpl {
+import kr.or.connect.reservation.dao.ProductDao;
+import kr.or.connect.reservation.dto.ProductDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+
+    private ProductDao productDao;
+
+    @Autowired
+    public ProductServiceImpl(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @Override
+    public List<ProductDto> getProducts(int id, int start, int limit) {
+        return productDao.selectById(id, start, limit);
+    }
 }
