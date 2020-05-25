df2ce3ec5c7ab43e79320bce93515f7661b5d926
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-25 18:54:09 +0900

    add getCategoryCount

--- reservation/src/main/java/kr/or/connect/reservation/service/CategoryService.java
+++ reservation/src/main/java/kr/or/connect/reservation/service/CategoryService.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface CategoryService {
     List<CategoryDto> getCategories();
+    int getCategoryCount(int id);
 }
--- reservation/src/main/java/kr/or/connect/reservation/service/CategoryServiceImpl.java
+++ reservation/src/main/java/kr/or/connect/reservation/service/CategoryServiceImpl.java
@@ -21,4 +21,9 @@ public class CategoryServiceImpl implements CategoryService {
     public List<CategoryDto> getCategories() {
         return categoryDao.selectAll();
     }
+
+    @Override
+    public int getCategoryCount(int id) {
+        return categoryDao.selectCount(id);
+    }
 }
