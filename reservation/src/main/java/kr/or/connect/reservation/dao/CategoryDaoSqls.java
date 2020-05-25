2991c72fb3f4f17189ff8812d0c5b1cbacbd9304
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-25 18:53:28 +0900

    add SELECT_COUNT_SQL

--- reservation/src/main/java/kr/or/connect/reservation/dao/CategoryDaoSqls.java
+++ reservation/src/main/java/kr/or/connect/reservation/dao/CategoryDaoSqls.java
@@ -1,5 +1,6 @@
 package kr.or.connect.reservation.dao;
 
 public class CategoryDaoSqls {
-    public static final String SELECT_SQL = "select t1.id, t1.name, count(*) as count from category as t1 join product as t2 on t1.id = t2.category_id group by t1.id";
+    public static final String SELECT_ALL_SQL = "select t1.id, t1.name, count(*) count from category t1 join product t2 on t1.id = t2.category_id group by t1.id";
+    public static final String SELECT_COUNT_SQL = "select count(*) from category t1 join product t2 on t1.id = t2.category_id where t1.id = :id group by t1.id";
 }
