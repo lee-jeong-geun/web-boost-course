b5a4e2801a261f2b220de11094ddc28a901040dc
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-25 18:36:53 +0900

    init ProductController

---
+++ reservation/src/main/java/kr/or/connect/reservation/controller/ProductController.java
@@ -0,0 +1,10 @@
+package kr.or.connect.reservation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/products")
+public class ProductController {
+
+}
