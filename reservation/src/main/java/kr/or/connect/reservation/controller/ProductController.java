997218abf02bb39a1149311d528eecc82e108921
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-27 01:31:39 +0900

    add constructor, GetMapping

--- reservation/src/main/java/kr/or/connect/reservation/controller/ProductController.java
+++ reservation/src/main/java/kr/or/connect/reservation/controller/ProductController.java
@@ -1,10 +1,46 @@
 package kr.or.connect.reservation.controller;
 
+import kr.or.connect.reservation.dto.ProductDto;
+import kr.or.connect.reservation.service.CategoryService;
+import kr.or.connect.reservation.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/api/products")
 public class ProductController {
 
+    private CategoryService categoryService;
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(CategoryService categoryService, ProductService productService) {
+        this.categoryService = categoryService;
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public Map<String, Object> products(@RequestParam(value = "categoryId", required = false, defaultValue = "0") int categoryId,
+                                        @RequestParam(value = "start", required = false, defaultValue = "0") int start) {
+        int totalCount = 0;
+        List<ProductDto> list = null;
+        Map<String, Object> map = new HashMap<>();
+        if (categoryId == 0) {
+            totalCount = categoryService.getCategoryAllCount();
+            list = productService.getProductsAll(start, start + 4);
+        } else {
+            totalCount = categoryService.getCategoryCount(categoryId);
+            list = productService.getProducts(categoryId, start, start + 4);
+        }
+        map.put("totalCount", totalCount);
+        map.put("items", list);
+        return map;
+    }
 }
