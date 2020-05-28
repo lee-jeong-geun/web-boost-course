36ad3b09d281b6c1103dd911e3419d552916bdf3
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-28 16:34:30 +0900

    add PromotionDto

---
+++ reservation/src/main/java/kr/or/connect/reservation/dto/PromotionDto.java
@@ -0,0 +1,40 @@
+package kr.or.connect.reservation.dto;
+
+public class PromotionDto {
+    private int id;
+    private int productId;
+    private String productImageUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductImageUrl() {
+        return productImageUrl;
+    }
+
+    public void setProductImageUrl(String productImageUrl) {
+        this.productImageUrl = productImageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "PromotionDto{" +
+                "id=" + id +
+                ", productId=" + productId +
+                ", productImageUrl='" + productImageUrl + '\'' +
+                '}';
+    }
+}
