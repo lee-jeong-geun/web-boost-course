42f685d5d061a270143e2feef7386746dab5549b
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-28 18:53:07 +0900

    add GetMapping

---
+++ reservation/src/main/java/kr/or/connect/reservation/controller/PromotionController.java
@@ -0,0 +1,35 @@
+package kr.or.connect.reservation.controller;
+
+import kr.or.connect.reservation.dto.PromotionDto;
+import kr.or.connect.reservation.service.PromotionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path="/api/promotions")
+public class PromotionController {
+
+    private PromotionService promotionService;
+
+    @Autowired
+    public PromotionController(PromotionService promotionService) {
+        this.promotionService = promotionService;
+    }
+
+    @GetMapping
+    public Map<String, Object> getPromotions() {
+        Map<String, Object> map = new HashMap<>();
+        List<PromotionDto> list = promotionService.getPromotions();
+        for(PromotionDto promotionDto : list) {
+            promotionDto.setProductImageUrl("img/" + promotionDto.getProductImageUrl());
+        }
+        map.put("items", list);
+        return map;
+    }
+}
