69d7c5faa46b2bfab3b77a11f5b6dc206d65a662
lee-jeong-geun <ljk0411jg@naver.com>
2020-05-31 23:49:49 +0900

    add makeRequest

---
+++ reservation/src/main/webapp/resources/js/mainpage.js
@@ -0,0 +1,22 @@
+const categoryList = document.querySelector(".event_tab_lst");
+
+function makeRequest(method, url, body) {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+
+        request.onreadystatechange = function () {
+            if (request.readyState !== XMLHttpRequest.DONE) {
+                return;
+            } else if (request.status === 200) {
+                resolve(request.response);
+            } else {
+                reject(request.status);
+            }
+        }
+        request.open(method, url);
+        request.setRequestHeader('Content-type', 'application/json');
+        request.send(JSON.stringify(body));
+    })
+}
+
+
