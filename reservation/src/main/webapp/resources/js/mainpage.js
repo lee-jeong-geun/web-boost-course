c6f855743d7e4945133af753a6fc0bca9a820a84
lee-jeong-geun <ljk0411jg@naver.com>
2020-06-01 04:04:01 +0900

    add insertDataTemplate, loadCategory

--- reservation/src/main/webapp/resources/js/mainpage.js
+++ reservation/src/main/webapp/resources/js/mainpage.js
@@ -1,5 +1,3 @@
-const categoryList = document.querySelector(".event_tab_lst");
-
 function makeRequest(method, url, body) {
     return new Promise((resolve, reject) => {
         const request = new XMLHttpRequest();
@@ -15,8 +13,30 @@ function makeRequest(method, url, body) {
         }
         request.open(method, url);
         request.setRequestHeader('Content-type', 'application/json');
+        request.responseType = 'json';
         request.send(JSON.stringify(body));
     })
 }
 
+function insertDataTemplate(node, template, data) {
+    let result = [];
+    result = data["items"].map(x => {
+        return template.replace("{id}", x["id"]).replace("{name}", x["name"]);
+    });
+    result.forEach(x => {
+        node.innerHTML += x.trim();
+    });
+}
 
+async function loadCategory() {
+    try {
+        const data = await makeRequest("GET", "/reservation/api/categories", '');
+        const categoryList = document.querySelector(".event_tab_lst");
+        const template = document.querySelector("#categoryItem").innerHTML;
+        insertDataTemplate(categoryList, template, data);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+loadCategory();
