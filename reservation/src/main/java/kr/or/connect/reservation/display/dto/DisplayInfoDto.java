130f6a332dc349a28bf50e6b53658b0411ffad2e
lee-jeong-geun <ljk0411jg@naver.com>
2020-07-07 22:07:47 +0900

    add DisplayInfoDto

---
+++ reservation/src/main/java/kr/or/connect/reservation/display/dto/DisplayInfoDto.java
@@ -0,0 +1,172 @@
+package kr.or.connect.reservation.display.dto;
+
+import java.util.Date;
+
+public class DisplayInfoDto {
+    private int productId;
+    private int categoryId;
+    private int displayInfoId;
+    private String categoryName;
+    private String productDescription;
+    private String productContent;
+    private String productEvent;
+    private String openingHours;
+    private String placeName;
+    private String placeLot;
+    private String placeStreet;
+    private String telephone;
+    private String homepage;
+    private String email;
+    private Date createDate;
+    private Date modifyDate;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public int getDisplayInfoId() {
+        return displayInfoId;
+    }
+
+    public void setDisplayInfoId(int displayInfoId) {
+        this.displayInfoId = displayInfoId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductContent() {
+        return productContent;
+    }
+
+    public void setProductContent(String productContent) {
+        this.productContent = productContent;
+    }
+
+    public String getProductEvent() {
+        return productEvent;
+    }
+
+    public void setProductEvent(String productEvent) {
+        this.productEvent = productEvent;
+    }
+
+    public String getOpeningHours() {
+        return openingHours;
+    }
+
+    public void setOpeningHours(String openingHours) {
+        this.openingHours = openingHours;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public String getPlaceLot() {
+        return placeLot;
+    }
+
+    public void setPlaceLot(String placeLot) {
+        this.placeLot = placeLot;
+    }
+
+    public String getPlaceStreet() {
+        return placeStreet;
+    }
+
+    public void setPlaceStreet(String placeStreet) {
+        this.placeStreet = placeStreet;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    @Override
+    public String toString() {
+        return "DisplayInfoDto{" +
+                "productId=" + productId +
+                ", categoryId=" + categoryId +
+                ", displayInfoId=" + displayInfoId +
+                ", categoryName='" + categoryName + '\'' +
+                ", productDescription='" + productDescription + '\'' +
+                ", productContent='" + productContent + '\'' +
+                ", productEvent='" + productEvent + '\'' +
+                ", openingHours='" + openingHours + '\'' +
+                ", placeName='" + placeName + '\'' +
+                ", placeLot='" + placeLot + '\'' +
+                ", placeStreet='" + placeStreet + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", homepage='" + homepage + '\'' +
+                ", email='" + email + '\'' +
+                ", createDate=" + createDate +
+                ", modifyDate=" + modifyDate +
+                '}';
+    }
+}
