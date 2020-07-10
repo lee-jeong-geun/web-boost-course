03ee5dbd74fde319efff0ee51c592d59c0b4b4c1
lee-jeong-geun <ljk0411jg@naver.com>
2020-07-10 16:09:26 +0900

    add selectByProductId

---
+++ reservation/src/main/java/kr/or/connect/reservation/product/dao/ProductPriceDao.java
@@ -0,0 +1,29 @@
+package kr.or.connect.reservation.product.dao;
+
+import kr.or.connect.reservation.product.dto.ProductPriceDto;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static kr.or.connect.reservation.product.dao.ProductPriceDaoSqls.*;
+
+@Repository
+public class ProductPriceDao {
+    private NamedParameterJdbcTemplate jdbc;
+    private RowMapper<ProductPriceDto> rowMapper = BeanPropertyRowMapper.newInstance(ProductPriceDto.class);
+
+    public ProductPriceDao(DataSource dataSource) {
+        jdbc = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    public List<ProductPriceDto> selectByProductId(int productId) {
+        Map<String, ?> param = Collections.singletonMap("id", productId);
+        return jdbc.query(SELECT_BY_PRODUCTID_SQL, param, rowMapper);
+    }
+}
