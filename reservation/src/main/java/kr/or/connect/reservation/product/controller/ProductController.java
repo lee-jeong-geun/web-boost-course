dd7d88b9fb01fd76441591d63415726d6d30c5c3
lee-jeong-geun <ljk0411jg@naver.com>
2020-07-10 16:07:35 +0900

    add product productPrices

--- reservation/src/main/java/kr/or/connect/reservation/product/controller/ProductController.java
+++ reservation/src/main/java/kr/or/connect/reservation/product/controller/ProductController.java
@@ -1,8 +1,8 @@
 package kr.or.connect.reservation.product.controller;
 
 import kr.or.connect.reservation.product.dto.ProductDto;
+import kr.or.connect.reservation.product.service.ProductPriceService;
 import kr.or.connect.reservation.product.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -15,10 +15,11 @@ public class ProductController {
     private static final int PRODUCT_LIMIT = 4;
 
     private ProductService productService;
+    private ProductPriceService productPriceService;
 
-    @Autowired
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, ProductPriceService productPriceService) {
         this.productService = productService;
+        this.productPriceService = productPriceService;
     }
 
     @GetMapping
@@ -42,8 +43,9 @@ public class ProductController {
 
     @GetMapping
     @RequestMapping(path = "/{displayInfoId}")
-    public Map<String, Object> product(@PathVariable(name="displayInfoId") int id) {
+    public Map<String, Object> product(@PathVariable(name = "displayInfoId") int id) {
         Map<String, Object> map = new HashMap<>();
+        map.put("productPrices", productPriceService.getProductPrices(id));
         return map;
     }
 }
