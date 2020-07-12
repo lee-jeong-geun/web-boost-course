af394e37ffe1714b5e1d5243d6bb7fd131e9b3b5
lee-jeong-geun <ljk0411jg@naver.com>
2020-07-13 04:07:11 +0900

    modify getReservationUserComments

--- reservation/src/main/java/kr/or/connect/reservation/reservation/service/ReservationUserCommentServiceImpl.java
+++ reservation/src/main/java/kr/or/connect/reservation/reservation/service/ReservationUserCommentServiceImpl.java
@@ -1,6 +1,7 @@
 package kr.or.connect.reservation.reservation.service;
 
 import kr.or.connect.reservation.reservation.dao.ReservationUserCommentDao;
+import kr.or.connect.reservation.reservation.dao.ReservationUserCommentImageDao;
 import kr.or.connect.reservation.reservation.dto.ReservationUserCommentDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,16 +9,23 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ReservationUserCommentServiceImpl implements ReservationUserCommentService{
+public class ReservationUserCommentServiceImpl implements ReservationUserCommentService {
     private ReservationUserCommentDao reservationUserCommentDao;
+    private ReservationUserCommentImageDao reservationUserCommentImageDao;
 
     @Autowired
-    public ReservationUserCommentServiceImpl(ReservationUserCommentDao reservationUserCommentDao) {
+    public ReservationUserCommentServiceImpl(ReservationUserCommentDao reservationUserCommentDao,
+                                             ReservationUserCommentImageDao reservationUserCommentImageDao) {
         this.reservationUserCommentDao = reservationUserCommentDao;
+        this.reservationUserCommentImageDao = reservationUserCommentImageDao;
     }
 
     @Override
     public List<ReservationUserCommentDto> getReservationUserComments(int productId) {
-        return reservationUserCommentDao.selectByProductId(productId);
+        List<ReservationUserCommentDto> commentList = reservationUserCommentDao.selectByProductId(productId);
+        for (ReservationUserCommentDto reservationUserCommentDto : commentList) {
+            reservationUserCommentDto.setCommentImages(reservationUserCommentImageDao.selectByCommentId(reservationUserCommentDto.getCommentId()));
+        }
+        return commentList;
     }
 }
