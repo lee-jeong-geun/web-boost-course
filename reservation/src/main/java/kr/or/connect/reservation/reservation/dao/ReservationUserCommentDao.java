a7da812d7d93eee59bb38bb2add5a60a4e895e39
lee-jeong-geun <ljk0411jg@naver.com>
2020-07-13 02:15:57 +0900

    add selectByProductId

---
+++ reservation/src/main/java/kr/or/connect/reservation/reservation/dao/ReservationUserCommentDao.java
@@ -0,0 +1,32 @@
+package kr.or.connect.reservation.reservation.dao;
+
+import kr.or.connect.reservation.reservation.dto.ReservationUserCommentDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static kr.or.connect.reservation.reservation.dao.ReservationUserCommentDaoSqls.*;
+
+@Repository
+public class ReservationUserCommentDao {
+    private NamedParameterJdbcTemplate jdbc;
+    private RowMapper<ReservationUserCommentDto> rowMapper = BeanPropertyRowMapper.newInstance(ReservationUserCommentDto.class);
+
+    @Autowired
+    public ReservationUserCommentDao(DataSource dataSource) {
+        this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    public List<ReservationUserCommentDto> selectByProductId(int productId) {
+        Map<String, Object> param = Collections.singletonMap("id", productId);
+        return jdbc.query(SELECT_BY_PRODUCTID_SQL, param, rowMapper);
+    }
+
+}
---
+++ reservation/src/main/java/kr/or/connect/reservation/reservation/dao/ReservationUserCommentDaoSqls.java
@@ -0,0 +1,24 @@
+package kr.or.connect.reservation.reservation.dao;
+
+public class ReservationUserCommentDaoSqls {
+    public static final String SELECT_BY_PRODUCTID_SQL = "select " +
+            "t3.id commentId, " +
+            "t1.id productId, " +
+            "t2.id reservationInfoId, " +
+            "t3.score score, " +
+            "t3.comment comment, " +
+            "t2.reservation_name reservationName, " +
+            "t2.reservation_tel reservationTelephone, " +
+            "t2.reservation_email reservationEmail, " +
+            "t2.reservation_date reservationDate, " +
+            "t3.create_date createDate, " +
+            "t3.modify_date modifyDate " +
+            "from " +
+            "product t1 " +
+            "join reservation_info t2 on " +
+            "t1.id = t2.product_id " +
+            "join reservation_user_comment t3 on " +
+            "t2.id = t3.reservation_info_id " +
+            "where t1.id = :id " +
+            "order by t3.id desc";
+}
