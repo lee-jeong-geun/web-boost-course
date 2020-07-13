8c266b39401a2eee23c17a019cd5c7a579e4b16b
lee-jeong-geun <ljk0411jg@naver.com>
2020-07-13 17:13:47 +0900

    add getDisplayInfo

---
+++ reservation/src/main/java/kr/or/connect/reservation/display/service/DisplayInfoService.java
@@ -0,0 +1,7 @@
+package kr.or.connect.reservation.display.service;
+
+import kr.or.connect.reservation.display.dto.DisplayInfoDto;
+
+public interface DisplayInfoService {
+    DisplayInfoDto getDisplayInfo(int displayInfoId);
+}
---
+++ reservation/src/main/java/kr/or/connect/reservation/display/service/DisplayInfoServiceImpl.java
@@ -0,0 +1,21 @@
+package kr.or.connect.reservation.display.service;
+
+import kr.or.connect.reservation.display.dao.DisplayInfoDao;
+import kr.or.connect.reservation.display.dto.DisplayInfoDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DisplayInfoServiceImpl implements DisplayInfoService{
+    private DisplayInfoDao displayInfoDao;
+
+    @Autowired
+    public DisplayInfoServiceImpl(DisplayInfoDao displayInfoDao) {
+        this.displayInfoDao = displayInfoDao;
+    }
+
+    @Override
+    public DisplayInfoDto getDisplayInfo(int displayInfoId) {
+        return displayInfoDao.selectById(displayInfoId);
+    }
+}
