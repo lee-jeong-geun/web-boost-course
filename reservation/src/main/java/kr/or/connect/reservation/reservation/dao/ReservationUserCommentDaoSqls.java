7c9a2b6c320b09e41af00b21fca6982816de8bfd
lee-jeong-geun <ljk0411jg@naver.com>
2020-07-15 03:04:43 +0900

    modify SELECT_BY_DISPLAYINFOID_SQL

--- reservation/src/main/java/kr/or/connect/reservation/reservation/dao/ReservationUserCommentDaoSqls.java
+++ reservation/src/main/java/kr/or/connect/reservation/reservation/dao/ReservationUserCommentDaoSqls.java
@@ -3,24 +3,27 @@ package kr.or.connect.reservation.reservation.dao;
 public class ReservationUserCommentDaoSqls {
     public static final String SELECT_BY_DISPLAYINFOID_SQL =
             "select " +
-            " t3.id commentId, " +
-            " t1.id productId, " +
+            " t1.id commentId, " +
+            " t3.id productId, " +
             " t2.id reservationInfoId, " +
-            " t3.score score, " +
-            " t3.comment comment, " +
+            " t1.score score, " +
+            " t1.comment comment, " +
             " t2.reservation_name reservationName, " +
             " t2.reservation_tel reservationTelephone, " +
             " t2.reservation_email reservationEmail, " +
             " t2.reservation_date reservationDate, " +
-            " t3.create_date createDate, " +
-            " t3.modify_date modifyDate " +
-            "from " +
-            " product t1 " +
+            " t1.create_date createDate, " +
+            " t1.modify_date modifyDate " +
+            "from  " +
+            " reservation_user_comment t1 " +
             "join reservation_info t2 on " +
-            " t1.id = t2.product_id " +
-            "join reservation_user_comment t3 on " +
-            " t2.id = t3.reservation_info_id " +
-            "where t1.id = :id " +
+            " t1.reservation_info_id = t2.id " +
+            "join product t3 on " +
+            " t1.product_id = t3.id " +
+            "join display_info t4 on  " +
+            " t4.product_id = t3.id " +
+            "where " +
+            " t4.id = :id " +
             "order by " +
-            " t3.id desc";
+            " t1.id desc";
 }
