cd3daba7eff4b34efc875b7de3496962e3b0eab8
lee-jeong-geun <ljk0411jg@naver.com>
2020-07-15 03:27:33 +0900

    add getReservationUserComments averageScore

--- reservation/src/main/java/kr/or/connect/reservation/reservation/service/ReservationUserCommentServiceImpl.java
+++ reservation/src/main/java/kr/or/connect/reservation/reservation/service/ReservationUserCommentServiceImpl.java
@@ -6,7 +6,9 @@ import kr.or.connect.reservation.reservation.dto.ReservationUserCommentDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ReservationUserCommentServiceImpl implements ReservationUserCommentService {
@@ -21,11 +23,21 @@ public class ReservationUserCommentServiceImpl implements ReservationUserComment
     }
 
     @Override
-    public List<ReservationUserCommentDto> getReservationUserComments(int displayInfoId) {
+    public Map<String, Object> getReservationUserComments(int displayInfoId) {
         List<ReservationUserCommentDto> commentList = reservationUserCommentDao.selectByDisplayInfoId(displayInfoId);
         for (ReservationUserCommentDto reservationUserCommentDto : commentList) {
             reservationUserCommentDto.setCommentImages(reservationUserCommentImageDao.selectByCommentId(reservationUserCommentDto.getCommentId()));
         }
-        return commentList;
+        int scoreSum = commentList.stream().mapToInt(ReservationUserCommentDto::getScore).sum();
+
+        double averageScore = 0;
+        if (!commentList.isEmpty()) {
+            averageScore = (double) scoreSum / commentList.size();
+        }
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("comments", commentList);
+        result.put("averageScore", averageScore);
+        return result;
     }
 }
