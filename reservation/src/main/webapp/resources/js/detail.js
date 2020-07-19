817465df6a95ddeab14c618d1a8d290b2b2f28d9
lee-jeong-geun <ljk0411jg@naver.com>
2020-07-19 23:22:40 +0900

    add loadIntroduce

--- reservation/src/main/webapp/resources/js/detail.js
+++ reservation/src/main/webapp/resources/js/detail.js
@@ -23,7 +23,14 @@
         let param = (new URL(document.location)).searchParams;
         let id = param.get('id');
         detailData = await makeRequest('GET', `/reservation/api/products/${id}`);
+        loadIntroduce(detailData.displayInfo);
+    }
+
+    function loadIntroduce({productContent}) {
+        const introduceBox = document.querySelector(".detail_info_lst .in_dsc");
+        introduceBox.innerHTML = productContent;
     }
 
     window.addEventListener("DOMContentLoaded", loadData);
 })();
+
