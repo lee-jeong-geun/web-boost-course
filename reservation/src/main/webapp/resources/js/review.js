edb860950923569221604285767f4dde27c2288d
lee-jeong-geun <ljk0411jg@naver.com>
2020-07-31 17:22:14 +0900

    add makeRequest

---
+++ reservation/src/main/webapp/resources/js/review.js
@@ -0,0 +1,19 @@
+(() => {
+    const makeRequest = (method, url, body) => new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+
+        request.onreadystatechange = () => {
+            if (request.readyState !== XMLHttpRequest.DONE) {
+                return;
+            } else if (request.status === 200) {
+                resolve(request.response);
+            } else {
+                reject(request.status);
+            }
+        }
+        request.open(method, url);
+        request.setRequestHeader('Content-type', 'application/json');
+        request.responseType = 'json';
+        request.send(JSON.stringify(body));
+    });
+})
